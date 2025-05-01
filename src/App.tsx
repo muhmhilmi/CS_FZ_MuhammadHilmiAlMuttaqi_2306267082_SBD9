61b7fe5dd088f87b93e43beca7da19ab78fbcd75
Muhammad Hilmi <muhammadhilmi.almuttaqib@gmail.com>
2025-05-01 20:32:29 +0700

    CS_FZ_MuhammadHilmiAlMuttaqi_2306267082_SBD9

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
